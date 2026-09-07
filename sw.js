a4108ccc9b21b21b8158d0df33bc78a8202bcb1a
razborkament-cpu <razborkament@gmail.com>
2026-09-07 14:06:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = "student-diary-v1";
 
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
